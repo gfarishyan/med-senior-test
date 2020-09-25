c66c6c89728c0f0cfa66a3f8c361633b3515e080
Grigor Farishyan <farishyan@gmail.com>
2020-09-25 18:59:03 +0200

    Enable sorters for columns

--- js/med-senior-test.js
+++ js/med-senior-test.js
@@ -1,14 +1,23 @@
 jQuery( function( $ ) {
   $.each(medSeniorTestSettings.items, function(index, item) {
     let cols = [];
+    let sorts = (item.sorts.length > 0) ? item.sorts : [];
     for (i in item.columns) {
-      cols.push({"data": item.columns[i]});
+      let col = {"data": item.columns[i], "orderable": false};
+      for (j in sorts) {
+        if (sorts[j][0] == i) {
+          col.orderable = true;
+          break;
+        }
+      }
+      cols.push(col);
     }
     let $target = $(item.target);
     $(item.id).DataTable({
       processing: true,
       serverSide: true,
       columns: cols,
+      order: (item.sorts.length > 0) ? item.sorts : false,
       ajax: {
         url: medSeniorTestSettings.ajax_url,
         type: 'POST',
